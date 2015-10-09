5a1fdb28b3a3c2993feb6fb0909cdfcfb4b718ff
imanemon <iman.emon@gmail.com>
2015-10-09 15:43:54 +0700

    update tadi
    a

--- application/views/menu/helpdesk/plugin.php
+++ application/views/menu/helpdesk/plugin.php
@@ -1,33 +1,33 @@
 <!-- BEGIN PLUGIN CSS -->
-<link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" media="screen"/>
-<link href="assets/plugins/bootstrap-select2/select2.css" rel="stylesheet" type="text/css" media="screen"/>
-<link href="assets/plugins/bootstrap-datepicker/css/datepicker.css" rel="stylesheet" type="text/css" />
-<link href="assets/plugins/bootstrap-timepicker/css/bootstrap-timepicker.css" rel="stylesheet" type="text/css" />
-<link href="assets/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.css" rel="stylesheet" type="text/css" />
-<link href="assets/plugins/boostrap-checkbox/css/bootstrap-checkbox.css" rel="stylesheet" type="text/css" media="screen"/>
-<link rel="stylesheet" href="assets/plugins/ios-switch/ios7-switch.css" type="text/css" media="screen">
+<link href="<?php echo base_url();?>assets/menu/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href="<?php echo base_url();?>assets/menu/plugins/bootstrap-select2/select2.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href="<?php echo base_url();?>assets/menu/plugins/bootstrap-datepicker/css/datepicker.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url();?>assets/menu/plugins/bootstrap-timepicker/css/bootstrap-timepicker.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url();?>assets/menu/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo base_url();?>assets/menu/plugins/boostrap-checkbox/css/bootstrap-checkbox.css" rel="stylesheet" type="text/css" media="screen"/>
+<link rel="stylesheet" href="<?php echo base_url();?>assets/menu/plugins/ios-switch/ios7-switch.css" type="text/css" media="screen">
 <!-- END PLUGIN CSS -->
 
 <!-- BEGIN CORE JS FRAMEWORK-->
-<script src="assets/plugins/jquery-1.8.3.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-ui/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
-<script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="assets/plugins/breakpoints.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/plugins/jquery-1.8.3.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/plugins/jquery-ui/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/plugins/breakpoints.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script>
 <!-- END CORE JS FRAMEWORK -->
 <!-- BEGIN PAGE LEVEL JS -->
-<script src="assets/plugins/jquery-scrollbar/jquery.scrollbar.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-block-ui/jqueryblockui.js" type="text/javascript"></script> 
-<script src="assets/plugins/jquery-numberAnimate/jquery.animateNumbers.js" type="text/javascript"></script>
-<script src="assets/plugins/pace/pace.min.js" type="text/javascript"></script>
-<script src="assets/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js" type="text/javascript"></script>
-<script src="assets/plugins/bootstrap-select2/select2.min.js" type="text/javascript"></script>
-<script src="assets/plugins/boostrap-form-wizard/js/jquery.bootstrap.wizard.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/plugins/jquery-scrollbar/jquery.scrollbar.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/plugins/jquery-block-ui/jqueryblockui.js" type="text/javascript"></script> 
+<script src="<?php echo base_url();?>assets/menu/plugins/jquery-numberAnimate/jquery.animateNumbers.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/plugins/pace/pace.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/plugins/bootstrap-datepicker/js/bootstrap-datepicker.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/plugins/bootstrap-select2/select2.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/plugins/boostrap-form-wizard/js/jquery.bootstrap.wizard.min.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
 <!-- END PAGE LEVEL PLUGINS -->
-<script src="assets/js/form_validations.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/js/form_validations.js" type="text/javascript"></script>
 <!-- BEGIN CORE TEMPLATE JS -->
-<script src="assets/js/core.js" type="text/javascript"></script>
-<script src="assets/js/demo.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/js/core.js" type="text/javascript"></script>
+<script src="<?php echo base_url();?>assets/menu/js/demo.js" type="text/javascript"></script>
 <!-- END CORE TEMPLATE JS -->
 <!-- END JAVASCRIPTS -->
